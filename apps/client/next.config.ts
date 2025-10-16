9d8e53251e241b51e72708f19ee166260ad12091
Uri Kiro <yu.shapkarin@gmail.com>
2025-10-16 20:41:52 +0300

    feat: add local proxy and vercel relatedProjects

--- apps/client/next.config.ts
+++ apps/client/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*', // Match any path under /api
+        destination: 'http://localhost:3000/:path*', // Proxy to your external API (TODO: add process.env condition)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
